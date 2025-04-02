a5d20225d4439ffbc31a0392dd09d06704e459fe
홍석주 <hsj202201@gmail.com>
2025-04-02 12:38:47 +0900

    initial commit

---
+++ src/main/java/com/example/logue/LogueApplication.java
@@ -0,0 +1,13 @@
+package com.example.logue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogueApplication.class, args);
+	}
+
+}
